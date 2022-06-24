68f1ac69f07cbc873b2f24d15d2a4705c04679a3
Cengiz Kilic <cngzklc@gmail.com>
2022-06-24 09:02:51 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hackerrank_Exapmles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
